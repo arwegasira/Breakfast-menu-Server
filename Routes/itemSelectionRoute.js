4ae9d88aeb1cb1fc2f8bf05264a4a13e57dd46e4
arwegasira <ac.rwegasira@gmail.com>
2025-03-10 19:43:27 +0200

    add order creation

--- Routes/itemSelectionRoute.js
+++ Routes/itemSelectionRoute.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { getAvailableItems } = require('../Controller/breakfastItemes')
+const { createOrder } = require('../Controller/orderController')
 
 router.get('/orderItems-selection', getAvailableItems)
+router.post('/create-order', createOrder)
 
 module.exports = router
